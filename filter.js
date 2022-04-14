af83389b3e640ee8d1bbf5270b5175fc96e121e5
MCHEVALIER1 <maxime.chevalier@ynov.com>
2022-04-14 09:48:11 +0200

    filter by : haire , eye , gender , powerStat

--- filter.js
+++ filter.js
@@ -13,3 +13,67 @@ Array.prototype.RaceSort = function() {
         return 0;
     })
 }
+
+Array.prototype.PowerSortByInt = function() {
+    this.sort(function(a, b) {
+        if (a.powerstats.intelligence < b.powerstats.intelligence) return -1;
+        if (a.powerstats.intelligence > b.powerstats.intelligence) return 1;
+        return 0;
+    })
+}
+Array.prototype.PowerSortByStr = function() {
+    this.sort(function(a, b) {
+        if (a.powerstats.strength < b.powerstats.strength) return -1;
+        if (a.powerstats.strength > b.powerstats.strength) return 1;
+        return 0;
+    })
+}
+Array.prototype.PowerSortBySpe = function() {
+    this.sort(function(a, b) {
+        if (a.powerstats.speed < b.powerstats.speed) return -1;
+        if (a.powerstats.speed > b.powerstats.speed) return 1;
+        return 0;
+    })
+}
+Array.prototype.PowerSortByDur = function() {
+    this.sort(function(a, b) {
+        if (a.powerstats.durability < b.powerstats.durability) return -1;
+        if (a.powerstats.durability > b.powerstats.durability) return 1;
+        return 0;
+    })
+}
+Array.prototype.PowerSortByPow = function() {
+    this.sort(function(a, b) {
+        if (a.powerstats.power < b.powerstats.power) return -1;
+        if (a.powerstats.power > b.powerstats.power) return 1;
+        return 0;
+    })
+}
+Array.prototype.PowerSortByCom = function() {
+    this.sort(function(a, b) {
+        if (a.powerstats.combat < b.powerstats.combat) return -1;
+        if (a.powerstats.combat > b.powerstats.combat) return 1;
+        return 0;
+    })
+}
+Array.prototype.GenderSort = function() {
+    this.sort(function(a, b) {
+        if (b.appearance.gender === "-" || a.appearance.gender < b.appearance.gender) return -1;
+        if (a.appearance.gender === "-" || a.appearance.gender > b.appearance.gender) return 1;
+        return 0;
+    })
+}
+Array.prototype.EyeColorSort = function() {
+    this.sort(function(a, b) {
+        if (b.appearance.eyeColor === "-" || a.appearance.eyeColor < b.appearance.eyeColor) return -1;
+        if (a.appearance.eyeColor === "-" || a.appearance.eyeColor > b.appearance.eyeColor) return 1;
+        return 0;
+    })
+}
+Array.prototype.HairColorSort = function() {
+    this.sort(function(a, b) {
+        if (b.appearance.hairColor === "-" || a.appearance.hairColor < b.appearance.hairColor) return -1;
+        if (a.appearance.hairColor === "-" || a.appearance.hairColor > b.appearance.hairColor) return 1;
+        return 0;
+    })
+}
--- test.js
+++ test.js
@@ -2,8 +2,8 @@ import * as filter from "./filter.js";
 
 let data = await fetch("https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/all.json")
 data = await data.json()
-data.RaceSort()
+data.PowerSortByStr()
     //console.log(data)
 for (let index = 0; index < data.length; index++) {
-    console.log(data[index].appearance.race)
+    console.log(data[index].powerstats.strength)
 }
