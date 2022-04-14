c53b32b5ece8997e4b846f185726a52ec724f85c
Flavio <flavio.cormerais@ynov.com>
2022-04-14 11:32:50 +0200

    ajout de la fonction de recherche

--- test.js
+++ test.js
@@ -1,8 +1,10 @@
 import * as filter from "./filter.js";
+import { search } from './search.js';
+
 
 let data = await fetch("https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/all.json")
 data = await data.json()
-data.PublisherSort()
-for (let index = 0; index < data.length; index++) {
-    console.log(data[index].biography.publisher)
-}
+// for (let index = 0; index < data.length; index++) {
+//     console.log(data[index].biography.publisher)
+// }
+console.log(search("iron", data))
