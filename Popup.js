46a96e5cd7339a0e95986d4695e6100152f8fa6c
MCHEVALIER1 <maxime.chevalier@ynov.com>
2022-04-20 10:06:39 +0200

    Popup Back

--- Popup.js
+++ Popup.js
@@ -5,38 +5,54 @@ export async function ShowPopup(id) {
     let persoData = await fetch("https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/id/" + id.toString() + ".json")
     persoData = await persoData.json()
 
-    //popup.replaceChildren()
-
     let img = document.getElementById("PopupImg")
     img.src = persoData.images.md
 
-    let name = document.createElement("h1")
+    let name = document.getElementById("PopupName")
     name.innerHTML = persoData.name
-    popup.appendChild(name)
+
+    let Bio = document.getElementById("Biography")
+    Bio.innerHTML = ""
+    for (let data in persoData.biography) {
+        Bio.innerHTML += data + " : " + persoData.biography[data] + "<br>"
+    }
+
+    let App = document.getElementById("Appearance")
+    App.innerHTML = ""
+    for (let data in persoData.appearance) {
+        App.innerHTML += data + " : " + persoData.appearance[data] + "<br>"
+    }
+
+    let PSt = document.getElementById("Powerstats")
+    PSt.innerHTML = ""
+    for (let data in persoData.powerstats) {
+        PSt.innerHTML += data + " : " + persoData.powerstats[data] + "<br>"
+    }
 
 
-    /*let biography = document.createElement("div")
+    let Oth = document.getElementById("Other")
+    Oth.innerHTML = ""
+    for (let data in persoData.connections) {
+        Oth.innerHTML += data + " : " + persoData.connections[data] + "<br>"
+    }
 
-    let appearance = document.createElement("div")
-
-    let powerstats = document.createElement("div")
-
-    let other = document.createElement("div")*/
 
 }
 
 function OpenPopupTab(id) {
     const listCat = [
-        [document.getElementById("BiographyCat")],
-        [document.getElementById("AppearanceCat")],
-        [document.getElementById("PowerstatsCat")],
-        [document.getElementById("OtherCat")]
+        [document.getElementById("BiographyCat"), document.getElementById("Biography")],
+        [document.getElementById("AppearanceCat"), document.getElementById("Appearance")],
+        [document.getElementById("PowerstatsCat"), document.getElementById("Powerstats")],
+        [document.getElementById("OtherCat"), document.getElementById("Other")]
     ]
     for (let i = 0; i < 4; i++) {
         if (id == i) {
             listCat[i][0].style.backgroundColor = "#FF1313"
+            listCat[i][1].style.display = "block"
         } else {
             listCat[i][0].style.backgroundColor = "#CD0000"
+            listCat[i][1].style.display = "none"
         }
     }
 }
