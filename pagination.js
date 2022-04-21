f7f2697828ce21579942907000b83870d9f0ee4c
Flavio <flavio.cormerais@ynov.com>
2022-04-21 08:15:46 +0200

    supprimer les fonctions inutiles

--- pagination.js
+++ pagination.js
@@ -1,21 +1,3 @@
-// export async function pagin (nb=50,last=0){
-//     let res = []
-//     let heroID = last+1
-//     let count = 0
-//     while(count<nb){
-//         try {
-//             var hero = await fetch('https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/id/'+heroID.toString()+'.json')
-//             var hero = await hero.json()
-//             res.push(hero)
-//             count++
-//             heroID++
-//         }catch{
-//             heroID++
-//         }
-//     }
-//     return res
-// }
-
 async function GetData(id){
     try {
         var hero = await fetch('https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/id/'+id.toString()+'.json')
@@ -36,57 +18,3 @@ export async function firstPage (nb=50) {
     })
     return data
 }
-
-// export async function lastPage(nb=50){
-//     let res = []
-//     let heroID = 731
-//     let count = 0
-//     while(count<nb){
-//         try {
-//             var hero = await fetch('https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/id/'+heroID.toString()+'.json')
-//             var hero = await hero.json()
-//             res.push(hero)
-//             count++
-//             heroID--
-//         }catch{
-//             heroID--
-//         }
-//     }
-//     return res.reverse()
-// }
-
-// export async function NextPage(nb=50,lastCurrentPage){
-//     let res = []
-//     let heroID = lastCurrentPage+1
-//     let count = 0
-//     while(count<nb){
-//         try {
-//             var hero = await fetch('https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/id/'+heroID.toString()+'.json')
-//             var hero = await hero.json()
-//             res.push(hero)
-//             count++
-//             heroID++
-//         }catch{
-//             heroID++
-//         }
-//     }
-//     return res
-// }
-
-// export async function PreviousPage(nb=50,lastCurrentPage){
-//     let res = []
-//     let heroID = lastCurrentPage1
-//     let count = 0
-//     while(count<nb){
-//         try {
-//             var hero = await fetch('https://rawcdn.githack.com/akabab/superhero-api/0.2.0/api/id/'+heroID.toString()+'.json')
-//             var hero = await hero.json()
-//             res.push(hero)
-//             count++
-//             heroID--
-//         }catch{
-//             heroID--
-//         }
-//     }
-//     return res.reverse()
-// }
